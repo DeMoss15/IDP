007cc303a20bce65429be9b03d039fafd8f4990b
Daniel <mossur15@gmail.com>
2018-10-24 12:43:59 +0300

    extended BaseRecyclerViewAdapter

--- app/src/main/java/com/demoss/idp/base/BaseRecyclerViewAdapter.kt
+++ app/src/main/java/com/demoss/idp/base/BaseRecyclerViewAdapter.kt
@@ -1,25 +1,68 @@
 package com.demoss.idp.base
 
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.RecyclerView
 
+abstract class BaseRecyclerViewAdapter<T,
+        VH : BaseRecyclerViewAdapter.BaseViewHolder<T>,
+        DiffUtilCallback : BaseRecyclerViewAdapter.BaseDiffUtilCallback<T>> : RecyclerView.Adapter<VH>() {
 
-abstract class BaseRecyclerViewAdapter<T, VH : RecyclerView.ViewHolder> : RecyclerView.Adapter<VH>() {
-
+    abstract val viewHolderFactory: (view: View) -> VH
+    abstract val layoutResId: Int
     abstract val data: MutableList<T>
+    abstract val diffUtilCallbackClazz: Class<DiffUtilCallback>?
 
+    // RV Adapter functions ============================================================================================
     override fun getItemCount(): Int = data.size
 
-    private inline fun <reified DiffCallback: MyDiffCallback<T>>dispatchData(list: List<T>) {
-        val result = DiffUtil.calculateDiff(DiffCallback(data, list))
-        data.clear()
-        data.addAll(list)
-        result.dispatchUpdatesTo(this)
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): VH = viewHolderFactory(
+        LayoutInflater.from(parent.context)
+            .inflate(layoutResId, parent, false)
+    )
+
+    override fun onBindViewHolder(holder: VH, position: Int) {
+        holder.bindData(data[position])
     }
 
-    fun addData(list: List<T>) {
+    // Basic DiffUtil usage functions ==================================================================================
+    open fun addData(list: List<T>) {
         dispatchData(data.toMutableList() + list.toMutableList())
     }
 
-    abstract class MyDiffCallback<T>(oldList: List<T>, newList: List<T>): DiffUtil.Callback()
+    open fun refresh(list: List<T>) {
+        data.clear()
+        notifyDataSetChanged()
+        dispatchData(list.toMutableList())
+    }
+
+    // DiffUtil applying ===============================================================================================
+    private fun dispatchData(list: List<T>) {
+        diffUtilCallbackClazz?.let {
+            // if clazz is not null
+            with(it.newInstance()) {
+                // callback instance
+                newList = list
+                oldList = data
+                with(DiffUtil.calculateDiff(this)) {
+                    // diff result
+                    data.clear()
+                    data.addAll(list)
+                    dispatchUpdatesTo(this@BaseRecyclerViewAdapter)
+                }
+            }
+        }
+    }
+
+    // Abstract classes ================================================================================================
+    abstract class BaseDiffUtilCallback<T>(var oldList: List<T>, var newList: List<T>) : DiffUtil.Callback() {
+        override fun getOldListSize(): Int = oldList.size
+        override fun getNewListSize(): Int = newList.size
+    }
+
+    abstract class BaseViewHolder<T>(val view: View) : RecyclerView.ViewHolder(view) {
+        abstract fun bindData(item: T)
+    }
 }
