9e948c96b217d2e682b22721cea640ff88680d6c
Daniel <mossur15@gmail.com>
2018-10-29 12:24:47 +0200

    fixed lint warnings

--- app/src/main/java/com/demoss/idp/data/repository/RepositoryDataModule.kt
+++ app/src/main/java/com/demoss/idp/data/repository/RepositoryDataModule.kt
@@ -2,6 +2,4 @@ package com.demoss.idp.data.repository
 
 import org.koin.dsl.module.module
 
-val repositoryDataModule = module {
-
-}
+val repositoryDataModule = module {}
--- app/src/main/java/com/demoss/idp/domain/model/ModelModule.kt
+++ app/src/main/java/com/demoss/idp/domain/model/ModelModule.kt
@@ -2,6 +2,4 @@ package com.demoss.idp.domain.model
 
 import org.koin.dsl.module.module
 
-val modelModule = module {
-
-}
+val modelModule = module {}
