c41ec6e3690950786bd514c7139dacc6ea81f7bf
Daniel <mossur15@gmail.com>
2018-11-01 12:51:22 +0200

    added okhttp profiler

--- app/src/main/java/com/demoss/idp/data/remote/NetworkModule.kt
+++ app/src/main/java/com/demoss/idp/data/remote/NetworkModule.kt
@@ -1,6 +1,8 @@
 package com.demoss.idp.data.remote
 
+import com.demoss.idp.BuildConfig
 import com.demoss.idp.Constants
+import com.itkacher.okhttpprofiler.OkHttpProfilerInterceptor
 import com.jakewharton.retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
 import okhttp3.OkHttpClient
 import org.koin.dsl.module.module
@@ -9,20 +11,23 @@ import retrofit2.converter.gson.GsonConverterFactory
 import java.util.concurrent.TimeUnit
 
 val networkModule = module {
-    single { // Retrofit
-        Retrofit.Builder()
-            .baseUrl(Constants.BASE_URL)
-            .addConverterFactory(GsonConverterFactory.create())
-            .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
-            .client(get())
-            .build()
+    single {
+        // Retrofit
+        Retrofit.Builder().apply {
+            baseUrl(Constants.BASE_URL)
+            addConverterFactory(GsonConverterFactory.create())
+            addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+            client(get())
+        }.build()
     }
-    single { // OkHttp
-        OkHttpClient.Builder()
-            .retryOnConnectionFailure(true)
-            .connectTimeout(60, TimeUnit.SECONDS)
-            .readTimeout(60, TimeUnit.SECONDS)
-            .writeTimeout(60, TimeUnit.SECONDS)
-            .build()
+    single {
+        // OkHttp
+        OkHttpClient.Builder().apply {
+            retryOnConnectionFailure(true)
+            connectTimeout(60, TimeUnit.SECONDS)
+            readTimeout(60, TimeUnit.SECONDS)
+            writeTimeout(60, TimeUnit.SECONDS)
+            if (BuildConfig.DEBUG) addInterceptor(OkHttpProfilerInterceptor())
+        }.build()
     }
 }
